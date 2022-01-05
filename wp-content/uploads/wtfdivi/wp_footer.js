e31e0abb2ca7fc75efdcd2d64195f42146d89556
Richard Schave <schavester@gmail.com>
2022-01-04 22:50:19 -0800

    all of emma's tweaks

--- wp-content/uploads/wtfdivi/wp_footer.js
+++ wp-content/uploads/wtfdivi/wp_footer.js
@@ -1,1 +1,1 @@
-jQuery(function($){});jQuery(function($){if(!$('#logo-text').length){$('#logo').after("<div id=\"db_title_and_tagline\"><h1 id=\"logo-text\">walkers<\/h1><h5 id=\"logo-tagline\" class=\"logo-tagline\">snappy tag line goes here<\/h5><\/div>");}});
+jQuery(function($){});jQuery(function($){if(!$('#logo-text').length){$('#logo').after("<div id=\"db_title_and_tagline\"><h1 id=\"logo-text\">Save Walker&#039;s Cafe<\/h1><h5 id=\"logo-tagline\" class=\"logo-tagline\"><\/h5><\/div>");}});
